eb1398150c4642ac88681da6cedb127ddaf622f5
ChienKangLu <gon01156109@gmail.com>
2018-09-01 00:44:16 +0800

    update

--- svm/SVM.py
+++ svm/SVM.py
@@ -2,17 +2,21 @@ import numpy as np
 
 
 class SVM:
-    def __init__(self,X):
+    def __init__(self, X):
         self.b = 0
         self.dim = X.shape[1]
         self.w = np.zeros(shape=[self.dim])
         self.N = X.shape[0]
         self.X = X
-        self.Y = [1]*int(self.N/2) + [-1]*int(self.N/2)
+        self.Y = [1] * int(self.N / 2) + [-1] * int(self.N / 2)
         self.Y = np.array(self.Y)
         self.alpha = np.random.random(self.N)
-        self.rate = 0.002
-        self.C = 50
+        self.rate = 0.005
+        self.C = 30
+        self.iter = 200
+        # print(self.X)
+        # print(self.Y)
+        # print(self.alpha)
 
     def pick(self):
         k = np.random.randint(0, self.N)
@@ -20,15 +24,16 @@ class SVM:
             k = np.random.randint(0, self.N)
         return k
 
-    def threshold(self,v):
+    def threshold(self, v):
         if v < 0:
             return 0
         if v > self.C:
             return self.C
+        return v
 
     def train(self):
         f = np.ones(self.N)
-        for iter in range(0, 100):
+        for iter in range(0, self.iter):
             hessian = np.empty(shape=[self.N, self.N])
             for i in range(0, self.N):
                 for j in range(0, self.N):
@@ -38,46 +43,57 @@ class SVM:
             dependent_sum = 0
             for i in range(0, self.N):
                 if i != k:
-                    self.alpha[i] = self.alpha[i] + self.rate * (d[i] - d[k] * self.Y[i] / self.Y[k])
-                    dependent_sum += self.alpha[i] * self.Y[i]
+                    self.alpha[i] = self.alpha[i] + self.rate * (d[i] + d[k] * (-self.Y[i] / self.Y[k]))
                     self.alpha[i] = self.threshold(self.alpha[i])
+                    dependent_sum += self.alpha[i] * self.Y[i]
             self.alpha[k] = (-1 / self.Y[k]) * dependent_sum
             self.alpha[k] = self.threshold(self.alpha[k])
-            print(iter)
+            self.getw()
+            self.getb()
+            print(iter,self.margin())
 
-        self.getw()
-        self.getb()
-        return self.alpha,self.w,self.b
-
+        print("alpha",self.alpha)
+        print("w",self.w)
+        print("b",self.b)
+        return self.alpha, self.w, self.b
 
     def getw(self):
+        self.w = np.zeros(shape=[self.dim])
         for i in range(0, self.N):
             self.w += self.alpha[i] * self.Y[i] * self.X[i]
 
     def getb(self):
         count = 0
+        self.b = 0
         for i in range(0, self.N):
             if self.alpha[i] > 0:
-                # b+=1/Y[i]-np.multiply(w.transpose(),X[i])
-                self.b += 1 / self.Y[i] - self.w.dot(self.X[i])
+                self.b += (1 / self.Y[i]) - self.w.dot(self.X[i])
                 count += 1
         self.b /= count
 
+    def margin(self):
+        return 2/(self.w.dot(self.w))
 
-    def centerLine(self):
+    def support_vector(self):
+        X=[]
+        Y=[]
+        count = 0
+        for i in range (0,self.N):
+            if self.alpha[i]!=0:
+                X.append(self.X[i][0])
+                Y.append(self.X[i][1])
+                print(self.X[i])
+                count+=1
+        print(count)
+        return X,Y
+
+    def decision_boundary(self, sign):
         X = []
         Y = []
-        for x1 in np.arange(-2, 4, 0.01):
-            x2 = (-self.w[0] * x1 - self.b) / self.w[1]
+        max = np.amax(self.X,axis=0)[0]
+        min = np.amin(self.X,axis=0)[0]
+        for x1 in np.arange(min, max, 0.01):
+            x2 = ((-1)*(self.w[0] * x1 +self.b - sign))/ self.w[1]
             X.append(x1)
             Y.append(x2)
         return X,Y
-
-    def supportLine(self,sign):
-        X = []
-        Y = []
-        for x1 in np.arange(-2, 4, 0.01):
-            x2 = (-self.w[0] * x1 - self.b + sign) / self.w[1]
-            X.append(x1)
-            Y.append(x2)
-        return
--- svm/svm_test.py
+++ svm/svm_test.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def draw(X, Y, color):
-    plt.scatter(X, Y, color=color, marker='o', alpha=0.5, linestyle='None', picker=True)
+def draw(X, Y, color,marker='o'):
+    plt.scatter(X, Y, color=color, marker=marker, alpha=0.5, linestyle='None', picker=True)
 
 
 def drawline(X, Y, color,linestyle):
@@ -14,19 +14,28 @@ def drawline(X, Y, color,linestyle):
 def main():
     dim = 2
     N = 100
-    data1 = np.random.multivariate_normal([-3, 3], [[3, 0], [0, 5]], int(N / 2))
+    np.random.seed(1000)
+    data1 = np.random.multivariate_normal([-5, 10], [[2, 0], [0, 2]], int(N / 2))
     draw(data1[:, 0], data1[:, 1], "red")
-    data2 = np.random.multivariate_normal([2, 2], [[0.2, 0], [0, 4]], int(N / 2))
+    data2 = np.random.multivariate_normal([2, 2], [[2, 0], [0, 2]], int(N / 2))
     draw(data2[:, 0], data2[:, 1], "blue")
     # plt.show()
     X = np.empty(shape=[0, dim])
     X = np.append(X, data1, axis=0)
     X = np.append(X, data2, axis=0)
-    print(X)
     svm = SVM(X)
     svm.train()
-    X,Y = svm.centerLine()
+    print("train over")
+    X,Y = svm.decision_boundary(0)
     drawline(X,Y,"black",'--')
+    X,Y = svm.decision_boundary(+1)
+    drawline(X,Y,"red",'-')
+    X,Y = svm.decision_boundary(-1)
+    drawline(X,Y,"blue",'-')
+    # X,Y = svm.support_vector()
+    # draw(X,Y,"black","x")
+
+    plt.show()
 
 
 if __name__ == "__main__":
